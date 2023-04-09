4e5fde13e34ea7a663f7aff8e03df8da018eba0c
Nitya <51813914+n-cyber@users.noreply.github.com>
2023-04-09 10:28:34 -0700

    Create front3.py

---
+++ Warm_up1/front3.py
@@ -0,0 +1,11 @@
+# Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+
+# front3('Java') → 'JavJavJav'
+# front3('Chocolate') → 'ChoChoCho'
+# front3('abc') → 'abcabcabc'
+
+def front3(str):
+  if(len(str)>=3):
+    return(3*str[0:3])
+  return 3*str 
