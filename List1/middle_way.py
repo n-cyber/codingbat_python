3a154961172fd3a898ba21881c84949c175a3955
Nitya <51813914+n-cyber@users.noreply.github.com>
2023-04-09 15:13:21 -0700

    Update middle_way.py

--- List1/middle_way.py
+++ List1/middle_way.py
@@ -6,8 +6,15 @@
 # middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
 # middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
 
+
+# Method1
 def middle_way(a, b):
   list=[]
   list.insert(0,b[1])
   list.insert(0,a[1])
   return list
+
+# Method2
+def middle_way(a, b):
+  array = [a[1],b[1]]
+  return array
