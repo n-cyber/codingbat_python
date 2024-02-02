d17de25c236beb108abe25cfc64537d392166b31
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-02-01 16:30:45 -0800

    Update pos_neg.py

--- Warm_up1/pos_neg.py
+++ Warm_up1/pos_neg.py
@@ -6,7 +6,14 @@
 # pos_neg(-1, 1, False) → True
 # pos_neg(-4, -5, True) → True
 
+#Method1
 def pos_neg(a, b, negative):
   if( negative and (a<0 and b <0)) or (not negative and a*b <0):
     return True
   return False
+
+#Method2
+def pos_neg(a, b, negative):
+  if negative:
+    return a<0 and b<0
+  return (a<0 and b>0) or (a>0 and b<0)
