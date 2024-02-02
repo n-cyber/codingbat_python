8fd8a57a54a10758f6531d970bebee6ac3d7056d
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-02-01 16:22:58 -0800

    Update front_back.py

--- Warm_up1/front_back.py
+++ Warm_up1/front_back.py
@@ -6,7 +6,17 @@
 # front_back('a') → 'a'
 # front_back('ab') → 'ba'
 
+#Method1
+def front_back(str):
+  n = len(str)
+  if n>2:
+    return str[-1]+str[1:-1]+str[0]
+  else:
+    return str[::-1] 
+
+#Method2
 def front_back(str):
   if(len(str)>1):
     return str[len(str)-1] + str[1:len(str)-1] + str[0]
   return str
+
