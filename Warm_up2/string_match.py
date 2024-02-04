f598e601d06fd30161e64c343f5ae6bc88a13540
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-02-03 18:06:04 -0800

    Update string_match.py

--- Warm_up2/string_match.py
+++ Warm_up2/string_match.py
@@ -6,9 +6,18 @@
 # string_match('abc', 'abc') → 2
 # string_match('abc', 'axc') → 0
 
+#Method1
 def string_match(a, b):
   count = 0
   for i in range(min(len(a)-1, len(b)-1)):
     if(a[i] == b[i]) and (a[i+1] == b[i+1]):
       count+=1
+  return count
+
+#Method2
+def string_match(a, b):
+  count = 0
+  for i in range(min(len(a)-1,len(b)-1)):
+    if a[i:i+2] == b[i:i+2]:
+      count +=1
   return count
