8a2031bbfbe8fa3ce19265859cc3686511f864b7
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-02-03 18:04:57 -0800

    Update array_123.py

--- Warm_up2/array_123.py
+++ Warm_up2/array_123.py
@@ -6,9 +6,16 @@
 # array123([1, 1, 2, 4, 1]) → False
 # array123([1, 1, 2, 1, 2, 3]) → True
 
-
+#Method1
 def array123(nums):
   for i in range(len(nums)-2):
     if(nums[i]==1 and nums[i+1]==2 and nums[i+2]==3):
+      return True 
+  return False
+
+#Method2
+def array123(nums):
+  for i in range(len(nums)-2):
+    if nums[i:i+3] == [1,2,3]:
       return True 
   return False
