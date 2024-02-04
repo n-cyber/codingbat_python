6c93af89e3d33c9a861edac2f8fb2a7ee3686e88
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-02-03 18:01:17 -0800

    Update last2.py

--- Warm_up2/last2.py
+++ Warm_up2/last2.py
@@ -6,11 +6,21 @@
 # last2('xaxxaxaxx') → 1
 # last2('axxxaaxx') → 2
 
-
+#Method1
 def last2(str):
   end_substring = str[-2:]
   count=0
   for i in range(len(str)-2):
     if(str[i:i+2] == end_substring):
+      count+=1
+  return count
+  
+#Method2
+def last2(str):
+  count = -1
+  if len(str) < 2:
+    return 0
+  for i in range(len(str)-1):
+    if str[i:i+2] == str[-2:]:
       count+=1
   return count
